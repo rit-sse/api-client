3b726e9241c29c4a4a8743845195897f83866c12
Brian Besmanoff <bbesmanoff@gmail.com>
2015-07-18 21:17:43 -0700

    Implement tip

--- src/api/tip.js
+++ src/api/tip.js
@@ -1,3 +1,25 @@
-module.exports = {
+var core = require('./core');
+var apiRoot = require('./config').apiRoot;
+var baseEndpoint = apiRoot + '/tips';
 
+module.exports = {
+  all: function() {
+    return core.get(baseEndpoint);
+  },
+
+  one: function(id) {
+    return core.get(baseEndpoint + '/' + id);
+  },
+
+  create: function(tip) {
+    return core.post(baseEndpoint, tip);
+  },
+
+  update: function(id, tip) {
+    return core.put(baseEndpoint + '/' + id, tip);
+  },
+
+  destroy: function(id) {
+    return core.delete(baseEndpoint + '/' + id);
+  }
 }

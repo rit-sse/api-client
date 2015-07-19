57c4580110e663f0e81c7420e6f6bf38dccbb7f1
kristenmills <kristen@kristen-mills.com>
2015-07-18 19:15:22 -0700

    Some empty files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  Committee: require('./api/committee'),
+  Event: require('./api/event'),
+  Lingo: require('./api/lingo'),
+  Member: require('./api/member'),
+  Membership: require('./api/membership'),
+  Mentor: require('./api/mentor'),
+  Officer: require('./api/officer'),
+  Term: require('./api/term'),
+  Tip: require('./api/tip')
+}

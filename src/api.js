5938a74c789a54a3a46b7dfb24de210095ddf886
Dylan Vuz <vuzdylan@gmail.com>
2017-02-21 21:08:19 -0500

    Stopped unwrapping data

--- src/api.js
+++ src/api.js
@@ -10,19 +10,20 @@ class API {
     if (actuallyAll) { // Quick way to get everything unpaginated
       query.page = 1;
       return this.core.get(this.resource + '?' + qs.stringify(query)).then(results => {
-          const pages = Math.ceil(results.total/results.perPage);
-          const r = [results];
-          for (let i = 2; i <= pages; i++) {
-            query.page = i;
-            r.push(this.core.get(this.resource + '?' + qs.stringify(query)));
-          }
-          return Promise.all(r);
-        })
-        .then(results => {
-          return results.reduce((data, cur) => {
+        const pages = Math.ceil(results.total/results.perPage);
+        const r = [results];
+        for (let i = 2; i <= pages; i++) {
+          query.page = i;
+          r.push(this.core.get(this.resource + '?' + qs.stringify(query)));
+        }
+        return Promise.all(r);
+      }).then(results => {
+        return {
+          data: results.reduce((data, cur) => {
             return data.concat(cur.data);
           }, []);
-        });
+        }
+      });
     }
     return this.core.get(this.resource + '?' + qs.stringify(query));
   }

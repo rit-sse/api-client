b53ea22b2e6f72a565c4e3553be862b9f5530b4a
Kristen Mills <kristen@kristen-mills.com>
2017-03-12 12:36:41 -0700

    Spell shifts right

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default function createAPI(apiRoot) {
   this.Mentors = new API('mentoring/mentors');
   this.Officers = new API('officers');
   this.Quotes = new API('qdb/quotes');
-  this.MentoringShifts = new API('mentoring/shfits');
+  this.Shifts = new API('mentoring/shifts');
   this.Specialties = new API('mentoring/specialties');
   this.Tags = new API('qdb/tags');
   this.Terms = new API('terms');
